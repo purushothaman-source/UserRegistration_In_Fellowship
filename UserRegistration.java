3678c911dc7e2e21fa1f57c98051e344f1745bbc
purushothaman-source <purushoth99jun@gmail.com>
2021-01-14 07:45:51 +0530

    ADDED : Code to validate password rule 1

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to UserRegistration");
         int options = 1;
-        while (options < 5) {
+        while (options < 6) {
 
             switch (options++) {
                 case 1:
@@ -23,6 +23,9 @@ public class UserRegistration {
                 case 4:
                     UserRegistrationMethods.mobile();
                     break;
+                case 5:
+                    UserRegistrationMethods.password();
+                    break;
             }
 
         }
@@ -36,6 +39,7 @@ class UserRegistrationMethods {
     static String lastName = "^[A-Z][a-zA-Z]{2,}$";
     static String email = "^[0-9a-zA-Z]+([.\\-_+][0-9a-zA-Z]+)*@[a-z0-9A-Z]+.[a-z]{2,4}([.][a-zA-Z]{2,})*$";
     static String mobile = "^91[ ][6-9][0-9]{9}$";
+    static String passwordR1 = "^[a-zA-Z0-9]{8,}$";
 
     public static void firstName() {
         while (true) {
@@ -101,4 +105,21 @@ class UserRegistrationMethods {
         }
 
     }
+
+    public static void password() {
+        while (true) {
+            System.out.println("Enter password with minimum 8 characters  : ");
+            text = input.nextLine();
+            Pattern pattern = Pattern.compile(passwordR1);
+            Matcher matcher = pattern.matcher(text);
+            if (matcher.matches()) {
+                System.out.println(">>>>Valid pattern>>>>");
+                break;
+            } else
+                System.out.println("******Invalid password , Try Again*******");
+
+        }
+
+    }
+
 }
