caea0cd9f5bc3fc05e63c0b8e425d3fc4deaa4fc
purushothaman-source <purushoth99jun@gmail.com>
2021-01-14 07:36:39 +0530

    ADDED : Code to validate Mobile Number

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,9 +8,9 @@ public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to UserRegistration");
         int options = 1;
-        while (options < 4) {
+        while (options < 5) {
 
-            switch (options) {
+            switch (options++) {
                 case 1:
                     UserRegistrationMethods.firstName();
                     break;
@@ -20,8 +20,11 @@ public class UserRegistration {
                 case 3:
                     UserRegistrationMethods.email();
                     break;
+                case 4:
+                    UserRegistrationMethods.mobile();
+                    break;
             }
-            options++;
+
         }
     }
 }
@@ -32,6 +35,7 @@ class UserRegistrationMethods {
     static String firstName = "^[A-Z][a-zA-Z]{2,}$";
     static String lastName = "^[A-Z][a-zA-Z]{2,}$";
     static String email = "^[0-9a-zA-Z]+([.\\-_+][0-9a-zA-Z]+)*@[a-z0-9A-Z]+.[a-z]{2,4}([.][a-zA-Z]{2,})*$";
+    static String mobile = "^91[ ][6-9][0-9]{9}$";
 
     public static void firstName() {
         while (true) {
@@ -80,5 +84,21 @@ class UserRegistrationMethods {
                 System.out.println("******Invalid email Pattern , Try Again*******");
             System.out.println(" ");
         }
+    }
+
+    public static void mobile() {
+        while (true) {
+            System.out.println("Enter mobilenumber with country code follow by space : ");
+            text = input.nextLine();
+            Pattern pattern = Pattern.compile(mobile);
+            Matcher matcher = pattern.matcher(text);
+            if (matcher.matches()) {
+                System.out.println(">>>>Valid pattern>>>>");
+                break;
+            } else
+                System.out.println("******Invalid mobile Format , Try Again*******");
+
+        }
+
     }
 }
